7167a92aa5f728b2d3b318d34a919c835a6d06a5
rahulkumar27382@gmail.com <rahulkumar27382@gmail.com>
2025-07-07 17:16:51 +0530

    TO__BE__REVERTED

--- client/src/app/leads/layout.tsx
+++ client/src/app/leads/layout.tsx
@@ -1,46 +1,29 @@
 "use client";
-
-import React, { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import Appsidebar from "../../../components/Appsidebar";
-import Workersidebar from "../../../components/Workersidebar";
 import Navbar from "../../../components/Navbar";
-import Workersidebar from "../../../components/Workersidebar";
 export default function ManagerLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   const pathname = usePathname();
-  const allCookies = document.cookie.split(";").map((cookie) => cookie.trim());
-  const found001 = allCookies.some((cookie) => cookie.startsWith("001"));
-  console.log("Found 001 cookie:", found001);
 
-  const allCookies = document.cookie.split(";").map((cookie) => cookie.trim());
-  const found001 = allCookies.some((cookie) => cookie.startsWith("001"));
-  console.log("found001", found001);
   //NOTE: Un-comment below logic to remove navbar from auth pages.
   const hideNavBar = pathname.startsWith('/manager/auth');
   // const hideNavBar = false;
   return (
     <div className="">
-      <SidebarProvider>
+         <SidebarProvider>
         {!hideNavBar && (
-          found001 ? <Workersidebar /> : <Appsidebar />
-        )}
-        <main className="w-full">
-          {!hideNavBar && <Navbar />}
-          {children}
-        </main>
-      </SidebarProvider>
-          found001 ? <Workersidebar /> : <Appsidebar />
-        )}
-        <main className="w-full">
-          {!hideNavBar && <Navbar />}
-          {children}
-        </main>
-      </SidebarProvider>
+            <Appsidebar />
+            )}
+            <main className="w-full">
+             {!hideNavBar &&(<Navbar />)} 
+                {children}
+            </main>
+          </SidebarProvider>
     </div>
   );
   //   return (
