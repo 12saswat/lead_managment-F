ebf5af67866f79c3556055d095048e13bde69f7c
rahulkumar27382@gmail.com <rahulkumar27382@gmail.com>
2025-07-08 18:05:23 +0530

    Fix : Axios

--- client/src/lib/Axios.ts
+++ client/src/lib/Axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-    baseURL: "http://localhost:8080/api/v1",
-    // baseURL:"https://api.lead.indibus.net/api/v1",
+    // baseURL: "http://localhost:8080/api/v1",
+    baseURL:"https://api.lead.indibus.net/api/v1",
     withCredentials: true
 })
 
