52337203cc8144b259758e0605edfdfff4c12f63
saswatjit ptra <saswatp813@gmail.com>
2025-09-16 17:52:44 +0530

    fix-cors

--- client/src/lib/Axios.ts
+++ client/src/lib/Axios.ts
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const Axios = axios.create({
-    // baseURL: "http://localhost:8080/api/v1",
-    baseURL:"https://api.lead.indibus.net/api/v1",
-    withCredentials: true
-})
+  // baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://api.lead.indibus.net/api/v1",
+  withCredentials: true,
+});
 
 export default Axios;
