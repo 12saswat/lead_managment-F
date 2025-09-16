3fe444cacf89459a0f9d09064e86ea8c79e83273
saswatjit ptra <saswatp813@gmail.com>
2025-09-16 18:28:23 +0530

    fix

--- client/src/lib/Axios.ts
+++ client/src/lib/Axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Axios = axios.create({
   // baseURL: "http://localhost:8080/api/v1",
-  baseURL: "https://lead-managment-b.onrender.com",
+  baseURL: "https://lead-managment-b.onrender.com/api/v1",
   withCredentials: true,
 });
 
